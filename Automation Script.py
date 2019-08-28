7c5c81269f740d78fb64b7423460bda6d554026d
mikechi <mike.chi@td.com>
2019-08-28 14:37:34 -0400

    update archive changes

--- Automation Script.py
+++ Automation Script.py
@@ -1,5 +1,7 @@
 Pick up file from Incoming folder
+backup the incomfing file in arhive folder
 Move file to Input folder
 Token Manager pickup file
 Generate output file
+also archive the output file.
 Send to EBX with exit code 0
