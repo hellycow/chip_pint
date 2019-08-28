4f6e90e8f9189ddefe3520e5fe33c09333e21f09
mikechi <mike.chi@td.com>
2019-08-28 14:33:37 -0400

    added split and merge

--- Automation Script.py
+++ Automation Script.py
@@ -1,5 +1,7 @@
 Pick up file from Incoming folder
+split the file in java and multos
 Move file to Input folder
 Token Manager pickup file
 Generate output file
+merge the java and multos file.
 Send to EBX with exit code 0
